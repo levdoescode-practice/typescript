c59587feda1ace35c950ea3e4b9f1fbf99c19497
LevDoesCode <levdoescode@gmail.com>
2024-09-30 12:53:41 -0500

    Passed props to test ttypes

--- 10-react-typing/01-proptypes/src/App.jsx
+++ 10-react-typing/01-proptypes/src/App.jsx
@@ -3,7 +3,13 @@ import Child from "./Child";
 
 export default function App() {
     return (
-        <Child name="John" age={30} test4={{ street: "York", area_code: 100001 }}>
+        <Child
+            name="John"
+            age={30}
+            test4={{ street: "York", area_code: 100001 }}
+            status="active"
+            test8={{ street: "Main", area_code: 123456, extra: "extra" }} // error
+        >
             Hi, this is John
         </Child>
     );
