6ca24ea77ff006d14f32bb6a71585ef0b9b2098f
LevDoesCode <levdoescode@gmail.com>
2024-09-30 13:31:00 -0500

    Child3 for children typed ReactNode

--- 10-react-typing/02-setup-props/src/App.tsx
+++ 10-react-typing/02-setup-props/src/App.tsx
@@ -1,5 +1,18 @@
 import Child from "./Child";
+import { Child2 } from "./Child2";
+import Child3 from "./Child3";
 
 export default function App() {
-    return <Child name="Leo" />;
+    return (
+        <>
+            <Child name="Leo" />
+            <Child2 name="Leo" />
+            <Child3>Children</Child3> // Ok
+            {/* <Child3> <p>Children here</p>  </Child3> // Not ok if the type is just string */}
+            <Child3>
+                <p>Children here</p>
+            </Child3>
+            {/* // Ok if the type is ReactNode */}
+        </>
+    );
 }
