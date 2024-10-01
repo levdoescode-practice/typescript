5c910916921551a96585b9f7c339bbfbbe3e4087
LevDoesCode <levdoescode@gmail.com>
2024-09-30 20:22:48 -0500

    Added Button2 which uses derived types

--- 10-react-typing/02-setup-props/src/App.tsx
+++ 10-react-typing/02-setup-props/src/App.tsx
@@ -1,7 +1,9 @@
 import { Button } from "./Button";
+import { Button2 } from "./Button2";
 import Child from "./Child";
 import { Child2 } from "./Child2";
 import Child3 from "./Child3";
+import Child4 from "./Child4";
 
 export default function App() {
     return (
@@ -17,6 +19,12 @@ export default function App() {
 
             <Button outline>This is a button</Button>
             {/* // Now we can pass any props that a button element can accept. */}
+
+            <Button2 outline city="Dublin" zip={123} >Button21</Button2>
+            <Button2 outline city="Dublin" zip={123} >Button22</Button2>
+            {/* // Button with custom props */}
+
+            <Child4 city="Dublin" zip={123} />
         </>
     );
 }
