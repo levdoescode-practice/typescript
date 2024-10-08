0d80d98f900d32738729e8f0eb1d56120b050bfc
LevDoesCode <levdoescode@gmail.com>
2024-10-08 16:18:01 -0500

    Created Modal for events

---
+++ 11-calendar-project/src/components/Modal.tsx
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import { createPortal } from "react-dom";
+
+type ModalProps = {
+    children: React.ReactNode;
+    isOpen: boolean;
+    onClose: () => void
+}
+
+export default function Modal({ children, isOpen, onClose }: ModalProps) {
+    useEffect(() => {
+        function handler(e: KeyboardEvent) {
+            if (e.key === "Escape") onClose();
+        }
+        document.addEventListener("keydown", handler);
+        return () => {
+            document.removeEventListener("keydown", handler);
+        }
+    }, [onClose]);
+
+    if (!isOpen) return null;
+
+    return createPortal(<div className="modal">
+        <div className="overlay" onClick={onClose} ></div>
+        <div className="modal-body">{children}</div>
+    </div>, document.getElementById("#modal-container") as HTMLElement);
+}
