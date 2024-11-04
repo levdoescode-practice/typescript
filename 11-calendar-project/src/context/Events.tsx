98d36b9d5fc8e83d931629178a2e82ba062f32f9
LevDoesCode <levdoescode@gmail.com>
2024-11-04 14:14:30 -0500

    Implemented removeEvent and updateEvent

--- 11-calendar-project/src/context/Events.tsx
+++ 11-calendar-project/src/context/Events.tsx
@@ -8,15 +8,14 @@ export type Event = {
     // color: "red" | "blue" | "green";
     color: (typeof EVENT_COLORS)[number];
     date: Date;
-} & (
-    | { allDay: false; startTime: string; endTime: string }
-    | { allDay: true; startTime?: never; endTime?: never }
-);
+} & ({ allDay: false; startTime: string; endTime: string } | { allDay: true; startTime?: never; endTime?: never });
 
 type EventsContext = {
     events: Event[];
     addEvent: (event: UnionOmit<Event, "id">) => void;
     removeEvent: (eventId: string) => void;
+    updateEvent: (id: string, event: UnionOmit<Event, "id">) => void;
+    deleteEvent: (id: string) => void;
 };
 
 export const Context = createContext<EventsContext | null>(null);
@@ -28,13 +27,23 @@ type EventsProviderProps = {
 export function EventsProvider({ children }: EventsProviderProps) {
     const [events, setEvents] = useState<Event[]>([]);
 
-    function addEvent(event: UnionOmit<Event, "id">) {
-        setEvents(e => [...e, { ...event, id: crypto.randomUUID() }]);
+    function addEvent(eventDetails: UnionOmit<Event, "id">) {
+        setEvents((e) => [...e, { ...eventDetails, id: crypto.randomUUID() }]);
+    }
+
+    function updateEvent(id: string, eventDetails: UnionOmit<Event, "id">) {
+        setEvents((e) => {
+            return e.map((event) => (event.id === id ? { id, ...eventDetails } : event));
+        });
     }
 
     function removeEvent(eventId: string) {
-        setEvents(e => e.filter(event => event.id !== eventId));
+        setEvents((e) => e.filter((event) => event.id !== eventId));
     }
 
-    return <Context.Provider value={{ events, addEvent, removeEvent }}>{ children }</Context.Provider>;
-};
+    function deleteEvent(id: string) {
+        setEvents((e) => e.filter((event) => event.id !== id));
+    }
+
+    return <Context.Provider value={{ events, addEvent, removeEvent, updateEvent, deleteEvent }}>{children}</Context.Provider>;
+}
