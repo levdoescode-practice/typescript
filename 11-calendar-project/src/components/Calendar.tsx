b0cdb84a9eea21a6f41846c4d67d792a8f04d519
LevDoesCode <levdoescode@gmail.com>
2024-11-04 13:48:53 -0500

    Implemented events showing up and sorted

--- 11-calendar-project/src/components/Calendar.tsx
+++ 11-calendar-project/src/components/Calendar.tsx
@@ -11,6 +11,8 @@ import {
     isToday,
     subMonths,
     addMonths,
+    isSameDay,
+    parse,
 } from "date-fns";
 import { formatDate } from "../utils/formatDate";
 import { cc } from "../utils/cc";
@@ -21,12 +23,13 @@ import { Event } from "../context/Events";
 
 export function Calendar() {
     const [selectedMonth, setSelectedMonth] = useState(new Date());
-    console.log(selectedMonth);
     const calendarDays = useMemo(() => {
         const firstWeekStart = startOfWeek(startOfMonth(selectedMonth));
         const lastWeekEnd = endOfWeek(endOfMonth(selectedMonth));
         return eachDayOfInterval({ start: firstWeekStart, end: lastWeekEnd });
     }, [selectedMonth]);
+
+    const { events } = useEvents();
 
     return (
         <div className="calendar">
@@ -52,7 +55,13 @@ export function Calendar() {
             </div>
             <div className="days">
                 {calendarDays.map((day, index) => (
-                    <CalendarDay key={day.getTime()} day={day} showWeekName={index < 7} selectedMonth={selectedMonth} />
+                    <CalendarDay
+                        key={day.getTime()}
+                        day={day}
+                        dayEvents={events.filter((event) => isSameDay(day, event.date))}
+                        showWeekName={index < 7}
+                        selectedMonth={selectedMonth}
+                    />
                 ))}
             </div>
         </div>
@@ -63,11 +72,27 @@ type CalendarDayProps = {
     day: Date;
     showWeekName: boolean;
     selectedMonth: Date;
+    dayEvents: Event[];
 };
 
-function CalendarDay({ day, showWeekName, selectedMonth }: CalendarDayProps) {
+function CalendarDay({ day, showWeekName, selectedMonth, dayEvents }: CalendarDayProps) {
     const [isNewEventModalOpen, setIsNewEventModalOpen] = useState(false);
-    const { events, addEvent } = useEvents();
+    const { addEvent } = useEvents();
+
+    const sortedEvents = useMemo(() => {
+        const timeToNumber = (time: string) => parseFloat(time.replace(":", "."));
+        return [...dayEvents].sort((a, b) => {
+            if (a.allDay && b.allDay) {
+                return 0;
+            } else if (a.allDay) {
+                return -1;
+            } else if (b.allDay) {
+                return 1;
+            } else {
+                return timeToNumber(a.startTime) - timeToNumber(b.startTime);
+            }
+        });
+    }, [dayEvents]);
     return (
         <div
             className={cc(
@@ -83,19 +108,14 @@ function CalendarDay({ day, showWeekName, selectedMonth }: CalendarDayProps) {
                     +
                 </button>
             </div>
-            {/* <div className="events">
-                <button className="all-day-event blue event">
-                    <div className="event-name">Short</div>
-                </button>
-                <button className="all-day-event green event">
-                    <div className="event-name">Long Event Name That Just Keeps Going</div>
-                </button>
-                <button className="event">
-                    <div className="color-dot blue"></div>
-                    <div className="event-time">7am</div>
-                    <div className="event-name">Event Name</div>
-                </button>
-            </div> */}
+            {dayEvents.length > 0 && (
+                <div className="events">
+                    {dayEvents.map((event) => (
+                        <CalendarEvent key={event.id} event={event} />
+                    ))}
+                </div>
+            )}
+
             <EventFormModal
                 date={day}
                 isOpen={isNewEventModalOpen}
@@ -103,6 +123,24 @@ function CalendarDay({ day, showWeekName, selectedMonth }: CalendarDayProps) {
                 onSumit={addEvent}
             />
         </div>
+    );
+}
+
+function CalendarEvent({ event }: { event: Event }) {
+    return (
+        <button className={cc("event", event.color, event.allDay && "all-day-event")}>
+            {event.allDay ? (
+                <div className="event-name">{event.name}</div>
+            ) : (
+                <>
+                    <div className={`color-dot ${event.color}`}></div>
+                    <div className="event-time">
+                        {formatDate(parse(event.startTime, "HH:mm", event.date), { timeStyle: "short" })}
+                    </div>
+                    <div className="event-name">{event.name}</div>
+                </>
+            )}
+        </button>
     );
 }
 
